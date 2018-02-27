f7d5dd56cc7d073cdef4a3c7d870678a2f7370de
Sean G Wilbur <sgwilbur@gmail.com>
2018-02-27 07:31:27 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# aws_splunk
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: aws_splunk
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
